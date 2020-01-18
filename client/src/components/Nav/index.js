b91ead3917591bc1e461bdc203ce521d0e12397c
Nico Loparco <nicoloparco75@gmail.com>
2020-01-18 12:26:42 -0500

    completed navbar and minor styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,15 +2,16 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className = "navbar navbar-dark bg-success">
-        <a className = "navbar-brand" href="#">
+    <nav className = "navbar navbar-dark bg-success py-0">
+        <a className = "navbar-brand" href="/">
             <h3 className = "font-weight-bold">SportsHub</h3>
         </a>
         <div className = "text-right">
-            <button className = "btn btn-light mr-1">Football<i class="fa fa-football-ball"></i></button>
-            <button className = "btn btn-light mr-1">Basketball</button>
-            <button className = "btn btn-light mr-1">Baseball</button>
-            <button className = "btn btn-light mr-1">Soccer</button>
+          <p class="text-light">Sort Articles and Posts by Sport</p>
+          <a href="/football"><i class="fas fa-football-ball mx-3 fa-2x pb-1" id="football" style={{color: "white"}}></i></a>
+          <a href="/basketball"><i class="fas fa-basketball-ball mx-3 fa-2x pb-1" style={{color: "white"}}></i></a>
+          <a href="/baseball"><i class="fas fa-baseball-ball mx-3 fa-2x pb-1" style={{color: "white"}}></i></a>
+          <a href="/soccer"><i class="fas fa-futbol mx-3 fa-2x pb-1" style={{color: "white"}}></i></a>
         </div>
     </nav>
   );
