42041f90ebc2235d6a00fdedb9ca8d2882e1c424
Dorian Bishop <dorianbishop427@gmail.com>
2020-01-21 18:53:55 -0500

    score scrapper page added

---
+++ Interfaces/ScoreScrapper.ts
@@ -0,0 +1,11 @@
+const express = require("express");
+const mlbEvents = require("./Interfaces/Events.ts");
+
+const app = express();
+const port = process.env.NODE_ENV === "production" ? 443 : 3000;
+
+app.use("/api/v1/sports", [mlbEvents]);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
