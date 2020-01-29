8b3987622b05dfcc2723d116ad2e5123445bfe5a
the 14th web <dennis.itua1@gmail.com>
2020-01-29 06:21:03 -0500

    updating server.js

--- server/index.js
+++ server/index.js
@@ -4,12 +4,12 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
-const router = require('./server/router');
+const router = require('./router');
 const mongoose = require('mongoose');
 // const cors = require('cors');  // we don't need it anymore, because we use proxy server instead
 
 // DB Setup (connect mongoose and instance of mongodb)
-mongoose.connect('mongodb://192.168.99.100/usersignupdb');
+mongoose.connect('mongodb://192.168.99.100/registrationpage');
 
 // App Setup (morgan and body-parser are middleware in Express)
 app.use(morgan('combined'));  // middleware for logging
