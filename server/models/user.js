931543241a5b2230643949ae90bd5595be2c43a8
the 14th web <dennis.itua1@gmail.com>
2020-01-29 09:38:36 -0500

    user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt-nodejs');
 
 // Define our model
 const userSchema = new Schema({
