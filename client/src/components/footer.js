aa82138771204a4882b9d80419c69335eb5f2e8a
the 14th web <dennis.itua1@gmail.com>
2020-01-29 09:03:34 -0500

    üpdating client

---
+++ client/src/components/footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Footer() {
+    return(
+        <footer className = "footer fixed-bottom mt-auto py-3 bg-success">
+            <div className = "container">
+                <span className = "text-light float-left">
+                    All Articles and Scores Belong to ESPN
+                </span>
+                <span className = "text-light float-right">
+                    Powered by React<i class="fab fa-react fa-lg mx-1"></i>
+                </span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
