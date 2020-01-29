cd612deaeff7c393fc15b0014bf61870e9335114
the 14th web <dennis.itua1@gmail.com>
2020-01-29 06:42:43 -0500

    updating client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import Nav from "./components/Nav"
-import Main from "./components/Main"
-import Footer from "./components/Footer"
-import './App.css';
+// import React from 'react';
+// import Nav from "./components/Nav"
+// import Main from "./components/Main"
+// import Footer from "./components/Footer"
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <div className="Main">
-      <Main />
-      </div>
-      <Footer />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <Nav />
+//       <div className="Main">
+//       <Main />
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
