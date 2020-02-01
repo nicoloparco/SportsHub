9d1cca01deb93e4fc80f93cfe5409219166637f4
thaislip <taber.haislip@gmail.com>
2020-02-01 12:59:54 -0500

    raw api data displaying on page for the articles section; need to make it look pretty and make it where new articles populate every time on refresh.

--- client/src/components/Article/index.js
+++ client/src/components/Article/index.js
@@ -1,45 +1,54 @@
-import React from "react"
+import React, {Component} from 'react';
 
-function Article() {
+class Article extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount(){
+
+    fetch('https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=878f0b51b8a143b1b6efda15a5c88184')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+    });
+
+  }
+
+  render(){
+    var{ isLoaded, items} = this.state;
+
+    if(!isLoaded){
+      return <div>loading...</div>;
+    }
+    else{
+
     return(
-           <div className = "container border 4px border-success my-3 rounded">
-                <div className = "articleContent">
-                    <h3>Titans to win Superbowl</h3>
-                        <img className = "image-thumbnail float-left mx-3" src="https://img.bleacherreport.net/img/images/photos/003/773/333/hi-res-4889005dc0494cbcc4a697f7de2e0aee_crop_north.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top" alt="football" height="100px" width="100px"></img>
-                        <p className = "text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pretium, magna eget consequat aliquam, quam ligula feugiat libero, eget efficitur libero nibh ac dui. 
-                        Proin a turpis at leo luctus venenatis et a mi. Vivamus rutrum nisl enim, sed feugiat quam feugiat in. Aenean non pulvinar metus, ut interdum nisl. 
-                        Vivamus ultricies tincidunt orci in efficitur. Vestibulum aliquam facilisis libero id elementum. Integer a metus eget odio vulputate accumsan. 
-                        Mauris lobortis a neque eu eleifend.</p>
-                </div>
-                    
-                <div className = "articleButtons my-1">
-                    <div className = "articleComments d-inline">
-                        <button className = "btn btn d-inline commentButton" style = {{backgroundColor: "#f0f1f5"}}>
-                            <span className = "d-inlne mx-1">
-                                <i class="fas fa-comments"></i>
-                            </span>
-                            Comment
-                        </button>
-                    </div>
-                    <div className = "articleSave d-inline">
-                        <button className = "btn btn d-inline saveButton" style = {{backgroundColor: "#f0f1f5"}}>
-                            <span className = "d-inlne mx-1">
-                                <i class="fas fa-bookmark"></i>
-                            </span>
-                            Save
-                        </button>
-                    </div>
-                    <div className = "articleShare d-inline">
-                        <button className = "btn btn d-inline shareButton" style = {{backgroundColor: "#f0f1f5"}}>
-                            <span className = "d-inlne mx-1">
-                                <i class="fas fa-retweet"></i>
-                            </span>
-                            Share
-                        </button>
-                    </div>
-                </div>
-            </div>
-    )
+      <div className="container border 4px border-success my-3 rounded">
+
+          <ul>
+            {items.articles.map((item,i) => (
+              <li key={i}>
+                <h3>Title</h3>: {item.title} | url: {item.url} |{item.urlToImage}
+              </li>
+
+            ))}
+          </ul>
+      </div>
+    );
+  }
+}
+
+
+
 }
 
 export default Article;
