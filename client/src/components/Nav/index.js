7b5d12b8b7a8c23d4d991a0d0c24ae930cdfeda6
Nico Loparco <nicoloparco75@gmail.com>
2020-02-08 19:12:26 -0500

    touched up navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,11 +7,10 @@ function Nav() {
             <h5 className = "font-weight-bold">SportsHub</h5>
         </a>
         <div className = "text-right">
-          <p className="text-light d-inline">Sort Articles and Posts by Sport: </p>
-          <a href="/football"><i class="fas fa-football-ball mx-3 fa-lg pb-1" style={{color: "white"}}></i></a>
-          <a href="/basketball"><i class="fas fa-basketball-ball mx-3 fa-lg pb-1" style={{color: "white"}}></i></a>
-          <a href="/baseball"><i class="fas fa-baseball-ball mx-3 fa-lg pb-1" style={{color: "white"}}></i></a>
-          <a href="/soccer"><i class="fas fa-futbol mx-3 fa-lg pb-1" style={{color: "white"}}></i></a>
+          <i class="fas fa-football-ball mx-3 fa-lg pb-1" style={{color: "white"}}></i>
+          <i class="fas fa-basketball-ball mx-3 fa-lg pb-1" style={{color: "white"}}></i>
+          <i class="fas fa-baseball-ball mx-3 fa-lg pb-1" style={{color: "white"}}></i>
+          <i class="fas fa-futbol mx-3 fa-lg pb-1" style={{color: "white"}}></i>
         </div>
     </nav>
   );
