ee28031fbbead31b7b6e8e955816851bbd9aa98b
Nico Loparco <nicoloparco75@gmail.com>
2020-02-11 18:36:54 -0500

    touch up

--- client/src/components/Scores/index.js
+++ client/src/components/Scores/index.js
@@ -6,7 +6,7 @@ import Soccer from "../soccer";
 
 function Scores () {
     return (
-        <div className = "col-md-5 my-3">
+        <div className = "col-md-5 my-3" style={{overflow:"scroll", height:"750px"}}>
                 <div className = "container my-3">   
                     <div className = "container border  my-5 rounded">
                         <h3 className = "text-center">SCORES</h3>
--- client/src/components/football.js
+++ client/src/components/football.js
@@ -3,22 +3,21 @@ import React from "react";
 function Football() {
     return(
         <div className = "container football border 4px border-success rounded">
-                                <h5>FOOTBALL</h5>
-                                    <div className = "container">
-                                    <h5>02/02/2020</h5>
-                                        <div className = "row">
-                                            <div className = "col-md-6">
-                                                <h5>San Francisco 49ers</h5>
-                                                <p><strong>31</strong></p>
-                                            </div>
-                                            <div className = "col-md-6">
-                                                <h5>Kansas City Chiefs</h5>
-                                                <p>20</p>
-                                            </div>
-                                            
-                                        </div>
-                                    </div>
-                            </div>
+            <h5>FOOTBALL</h5>
+            <div className = "container">
+                <div className = "row">
+                    <div className = "container border border-success rounded my-3">
+                    <p className = "date">02/02/2020</p>
+                        <div className = "container my-3">
+                            <p className = "d-inline mx-1">Kansas City Chiefs (KC)</p><span className = "scoreWinner mx-3 font-weight-bold">31</span>
+                        </div>
+                        <div className = "container my-3">
+                            <p className="d-inline mx-1">San Francisco 49'ers (SF)</p><span className = "scoreLoser mx-3">20</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
